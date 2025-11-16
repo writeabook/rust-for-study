6aac4ee1402242ffb20f2461a496d393babcc20d
Antonio Salsi <passy.linux@zresa.it>
2025-11-16 11:12:17 +0100

    feat: Update thread creation to use new method and enhance return value handling

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,6 +1,6 @@
 //! Esempio base di utilizzo di OSAL-RS
 
-
+use std::ffi::c_void;
 use std::sync::Arc;
 use osal_rs::{os_version, start_scheduler, stop_scheduler, Thread, ThreadDefaultPriority, ThreadTrait};
 
@@ -13,22 +13,40 @@ fn main() {
     println!();
 
 
-    let thread = Thread::create(
+    let thread = Thread::new(
         |_| {
+            let mut ret = 1;
             for _ in 0..5 {
+                ret += 1;
                 println!("Esecuzione del task di base...");
             }
-            stop_scheduler();
-            Ok(Arc::new(5))
+            Ok(Arc::new(ret))
         },
         "base_task",
         1024 * 16,
-        None,
         ThreadDefaultPriority::Normal,
     );
 
+
+
     match thread {
-        Ok(t) => {
+        Ok(mut t) => {
+
+            t.create(None).expect("panic message");
+
+            let mut ret_ptr = std::ptr::null_mut::<c_void>();
+            //let mut ret_ptr: *mut std::os::raw::c_void = null_mut();
+            t.join(&mut ret_ptr).unwrap();
+
+
+            if !ret_ptr.is_null() {
+                let ret_value = unsafe { *(ret_ptr as *const i32) };
+                unsafe { Arc::from_raw(ret_ptr as *mut i32); } // Libera la memoria
+                println!("Valore di ritorno del thread: {}", ret_value);
+            } else {
+                println!("Il thread non ha restituito alcun valore");
+            }
+
             println!("✓ Task creato con successo: {:?}", t);
         }
         Err(e) => {
