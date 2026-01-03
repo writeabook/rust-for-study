639d32f78b17fa3c6d1b321f047c504f7f79cb6d
Antonio Salsi <passy.linux@zresa.it>
2026-01-03 22:59:36 +0100

    feat: add FreeRTOS configuration access functions in config.rs

--- osal-rs/src/freertos/config.rs
+++ osal-rs/src/freertos/config.rs
@@ -17,13 +17,58 @@
  *
  ***************************************************************************/
 
-// Include auto-generated config from build.rs
-include!(concat!(env!("OUT_DIR"), "/config_generated.rs"));
+pub mod ffi {
+    use crate::freertos::types::{TickType, StackType};
+
+    unsafe extern "C" {
+        pub fn osal_rs_config_cpu_clock_hz() -> u64;
+        pub fn osal_rs_config_tick_rate_hz() -> TickType;
+        pub fn osal_rs_config_max_priorities() -> u32;
+        pub fn osal_rs_config_minimal_stack_size() -> StackType;
+        pub fn osal_rs_config_max_task_name_len() -> u32;
+    }
+}
 
 #[macro_export]
 macro_rules! tick_period_ms {
     () => {
         // CHECK (1000 / $crate::freertos::config::TICK_RATE_HZ)
-        ($crate::freertos::config::TICK_RATE_HZ)
+        (unsafe { $crate::freertos::config::ffi::osal_rs_config_tick_rate_hz() })
+    };
+}
+
+#[macro_export]
+macro_rules! tick_rate_hz {
+    () => {
+        (unsafe { $crate::freertos::config::ffi::osal_rs_config_tick_rate_hz() })
+    };
+}
+
+
+#[macro_export]
+macro_rules! cpu_clock_hz {
+    () => {
+        (unsafe { $crate::freertos::config::ffi::osal_rs_config_cpu_clock_hz() })
+    };
+}
+
+#[macro_export]
+macro_rules! max_priorities {
+    () => {
+        (unsafe { $crate::freertos::config::ffi::osal_rs_config_max_priorities() })
+    };
+}
+
+#[macro_export]
+macro_rules! minimal_stack_size {
+    () => {
+        ( unsafe { $crate::freertos::config::ffi::osal_rs_config_minimal_stack_size() })
+    };
+}   
+
+#[macro_export]
+macro_rules! max_task_name_len {
+    () => {
+        (unsafe { $crate::freertos::config::ffi::osal_rs_config_max_task_name_len() })
     };
 }
