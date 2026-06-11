516d75bf5006b01a732415c2a794d01dd04c0762
co1718 <1928803576@qq.com>
2026-06-11 15:57:47 +0800

    fix the problem of the single test in linux

--- osal-rs-tests/src/common/mod.rs
+++ osal-rs-tests/src/common/mod.rs
@@ -1,9 +1,21 @@
 pub mod duration_tests;
+
+// The following modules require types that are only implemented
+// in the FreeRTOS backend. They will be enabled for Linux backend
+// as each module is implemented.
+#[cfg(feature = "freertos")]
 pub mod api_surface;
+#[cfg(feature = "freertos")]
 pub mod event_group_tests;
+#[cfg(feature = "freertos")]
 pub mod mutex_tests;
+#[cfg(feature = "freertos")]
 pub mod queue_tests;
+#[cfg(feature = "freertos")]
 pub mod semaphore_tests;
+#[cfg(feature = "freertos")]
 pub mod system_tests;
+#[cfg(feature = "freertos")]
 pub mod thread_tests;
+#[cfg(feature = "freertos")]
 pub mod timer_tests;
--- osal-rs-tests/src/linux/mod.rs
+++ osal-rs-tests/src/linux/mod.rs
@@ -1,55 +1,6 @@
 use osal_rs::utils::Result;
 
-/// Run all currently-implemented Linux backend tests
-pub fn run_all_tests() -> Result<()> {
-    crate::common::duration_tests::run_all_tests()?;
-    // TODO: Add more modules as Linux backend implements them:
-    //   crate::common::mutex_tests::run_all_tests()?;
-    //   crate::common::semaphore_tests::run_all_tests()?;
-    //   crate::common::queue_tests::run_all_tests()?;
-    //   crate::common::event_group_tests::run_all_tests()?;
-    //   crate::common::system_tests::run_all_tests()?;
-    //   crate::common::thread_tests::run_all_tests()?;
-    //   crate::common::timer_tests::run_all_tests()?;
-    Ok(())
-}
-
-#[test]
-fn test_duration_to_ticks() {
-    crate::common::duration_tests::test_duration_to_ticks().unwrap();
-}
-
-#[test]
-fn test_duration_from_ticks() {
-    crate::common::duration_tests::test_duration_from_ticks().unwrap();
-}
-
-#[test]
-fn test_duration_conversion_roundtrip() {
-    crate::common::duration_tests::test_duration_conversion_roundtrip().unwrap();
-}
-
-#[test]
-fn test_duration_zero() {
-    crate::common::duration_tests::test_duration_zero().unwrap();
-}
-
-#[test]
-fn test_duration_one_second() {
-    crate::common::duration_tests::test_duration_one_second().unwrap();
-}
-
-#[test]
-fn test_duration_microseconds() {
-    crate::common::duration_tests::test_duration_microseconds().unwrap();
-}
-
-#[test]
-fn test_duration_large_value() {
-    crate::common::duration_tests::test_duration_large_value().unwrap();
-}
-
 #[test]
 fn test_run_all_tests() {
-    run_all_tests().unwrap();
+    crate::common::duration_tests::run_all_tests().unwrap();
 }
