e9782d6a7b8b8ea9d916e39309005cb9e513fc43
co1718 <1928803576@qq.com>
2026-06-11 13:29:11 +0800

    Remove LGPL headers from Linux backend files

--- osal-rs/src/linux/config.rs
+++ osal-rs/src/linux/config.rs
@@ -1,23 +1,3 @@
-/***************************************************************************
- *
- * osal-rs
- * Copyright (C) 2026 Antonio Salsi <passy.linux@zresa.it>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see <https://www.gnu.org/licenses/>.
- *
- ***************************************************************************/
-
 //! Configuration constants for the Linux OSAL backend.
 //!
 //! On Linux, 1 tick = 1 millisecond (TICK_PERIOD_MS = 1).
