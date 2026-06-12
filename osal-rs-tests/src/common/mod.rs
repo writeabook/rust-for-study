a7201e463b45ed6853bc4983c917c7c3df75ec2b
co1718 <1928803576@qq.com>
2026-06-12 15:37:02 +0800

    Update the mod.rs

--- osal-rs-tests/src/common/mod.rs
+++ osal-rs-tests/src/common/mod.rs
@@ -2,13 +2,13 @@ pub mod duration_tests;
 pub mod system_tests;
 pub mod mutex_tests;
 pub mod semaphore_tests;
+pub mod event_group_tests;
 
 // The following modules require types that are only implemented
 // in the FreeRTOS backend. They will be enabled for Linux backend
 // as each module is implemented.
 #[cfg(feature = "freertos")]
 pub mod api_surface;
-pub mod event_group_tests;
 #[cfg(feature = "freertos")]
 pub mod queue_tests;
 #[cfg(feature = "freertos")]
