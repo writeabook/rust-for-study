1c61a99aefac923917ee61fca6968955532bd4bb
co1718 <1928803576@qq.com>
2026-06-12 11:25:55 +0800

    Modify the test code

--- osal-rs-tests/src/common/mod.rs
+++ osal-rs-tests/src/common/mod.rs
@@ -1,4 +1,6 @@
 pub mod duration_tests;
+pub mod system_tests;
+pub mod mutex_tests;
 
 // The following modules require types that are only implemented
 // in the FreeRTOS backend. They will be enabled for Linux backend
@@ -7,12 +9,10 @@ pub mod duration_tests;
 pub mod api_surface;
 #[cfg(feature = "freertos")]
 pub mod event_group_tests;
-pub mod mutex_tests;
 #[cfg(feature = "freertos")]
 pub mod queue_tests;
 #[cfg(feature = "freertos")]
 pub mod semaphore_tests;
-pub mod system_tests;
 #[cfg(feature = "freertos")]
 pub mod thread_tests;
 #[cfg(feature = "freertos")]
--- osal-rs-tests/src/linux/mod.rs
+++ osal-rs-tests/src/linux/mod.rs
@@ -15,42 +15,42 @@ fn test_run_all_tests_mutex() {
     crate::common::mutex_tests::run_all_tests().unwrap();
 }
 
-#[test]
-fn test_system_get_tick_count() {
-    crate::common::system_tests::test_system_get_tick_count().unwrap();
-}
+// #[test]
+// fn test_system_get_tick_count() {
+//     crate::common::system_tests::test_system_get_tick_count().unwrap();
+// }
 
-#[test]
-fn test_system_get_current_time() {
-    crate::common::system_tests::test_system_get_current_time().unwrap();
-}
+// #[test]
+// fn test_system_get_current_time() {
+//     crate::common::system_tests::test_system_get_current_time().unwrap();
+// }
 
-#[test]
-fn test_system_delay() {
-    crate::common::system_tests::test_system_delay().unwrap();
-}
+// #[test]
+// fn test_system_delay() {
+//     crate::common::system_tests::test_system_delay().unwrap();
+// }
 
-#[test]
-fn test_system_delay_until() {
-    crate::common::system_tests::test_system_delay_until().unwrap();
-}
+// #[test]
+// fn test_system_delay_until() {
+//     crate::common::system_tests::test_system_delay_until().unwrap();
+// }
 
-#[test]
-fn test_system_check_timer() {
-    crate::common::system_tests::test_system_check_timer().unwrap();
-}
+// #[test]
+// fn test_system_check_timer() {
+//     crate::common::system_tests::test_system_check_timer().unwrap();
+// }
 
-#[test]
-fn test_system_time_conversion() {
-    crate::common::system_tests::test_system_time_conversion().unwrap();
-}
+// #[test]
+// fn test_system_time_conversion() {
+//     crate::common::system_tests::test_system_time_conversion().unwrap();
+// }
 
-#[test]
-fn test_system_multiple_delays() {
-    crate::common::system_tests::test_system_multiple_delays().unwrap();
-}
+// #[test]
+// fn test_system_multiple_delays() {
+//     crate::common::system_tests::test_system_multiple_delays().unwrap();
+// }
 
-#[test]
-fn test_system_time_monotonic() {
-    crate::common::system_tests::test_system_time_monotonic().unwrap();
-}
+// #[test]
+// fn test_system_time_monotonic() {
+//     crate::common::system_tests::test_system_time_monotonic().unwrap();
+// }
